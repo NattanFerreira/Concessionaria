348e5a8e1485e0e19373c769472c88bb8c72bdfa
Pedro Lucas <pedrollucasfr@gmail.com>
2025-07-05 17:45:14 -0300

    Adicionando a classe abstract Veiculo

---
+++ src/Models/Veiculo.java
@@ -0,0 +1,99 @@
+package Models;
+
+public abstract class Veiculo {
+    protected String modelo;
+    protected int numChassi;
+    protected double quilometragem;
+    protected double preco;
+    protected String cor;
+    protected int anoFabricacao;
+    protected String status;
+
+    public Veiculo(String modelo, int numChassi, double quilometragem, double preco, String cor, int anoFabricacao, String status) {
+        this.modelo = modelo;
+        this.numChassi = numChassi;
+        this.quilometragem = quilometragem;
+        this.preco = preco;
+        this.cor = cor;
+        this.anoFabricacao = anoFabricacao;
+        this.status = status;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getNumChassi() {
+        return numChassi;
+    }
+
+    public void setNumChassi(int numChassi) {
+        this.numChassi = numChassi;
+    }
+
+    public double getQuilometragem() {
+        return quilometragem;
+    }
+
+    public void setQuilometragem(double quilometragem) {
+        this.quilometragem = quilometragem;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setAnoFabricacao(int anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String exibirVeiculos() {
+        return "Modelo: " + this.modelo +
+                "\nChassi: " + this.numChassi +
+                "\nPreço: R$" + String.format("%.2f", this.preco) +
+                "\nCor: " + this.cor +
+                "\nAno: " + this.anoFabricacao +
+                "\nStatus: " + this.status;
+    }
+
+    public void cadastrarVeiculo() {
+        System.out.println("Veículo cadastrado");
+    }
+
+    public void removerVeiculo() {
+        System.out.println("Veículo removido");
+    }
+
+    public void atualizarVeiculo() {
+        System.out.println("Veículo atualizado");
+    }
+}
+
